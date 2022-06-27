1d2e7cdc5c97a1116c0d5bbba2d3bc0e763afb25
quilspace <emmanuel.eze.199075@unn.edu.ng>
2022-06-27 07:02:59 +0100

    dependencies and starter files commit

---
+++ I4G006274QBM/blog/views.py
@@ -0,0 +1,35 @@
+from audioop import reverse
+from pyexpat import model
+from typing import Generic
+from django.shortcuts import render
+
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Post
+class PostListView(ListView):
+    model  = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    field = "__all__"
+    success_url = reverse_lazy("blog:all")
+
